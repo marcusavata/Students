500f7a57ac480807593ea0bc0512fda5fd6e374f
Marcus <marcusandrei.avatajitei@ulbsibiu.ro>
2026-05-22 18:54:50 +0300

    Adaugat ArrayList si HashSet stundeti, metode pentru verificat daca un student e prezent + generat metodele equals() și hashCode() în clasa Student.

--- src/main/java/ro/ulbs/proiectaresoftware/students/Application.java
+++ src/main/java/ro/ulbs/proiectaresoftware/students/Application.java
@@ -1,4 +1,6 @@
 import ro.ulbs.proiectaresoftware.students.Student;
+import java.util.HashSet;
+import java.util.ArrayList;
 
 public static void main() {
     Student s1 = new Student(112,"Ioan","Popa","C21/1");
@@ -12,4 +14,14 @@ public static void main() {
     System.out.println(s3.toString());
     System.out.println(s4.toString());
     System.out.println(s5.toString());
+
+    ArrayList<Student> listaStudenti = new ArrayList<>();
+    listaStudenti.add(s1); listaStudenti.add(s2); listaStudenti.add(s3); listaStudenti.add(s4); listaStudenti.add(s5);
+
+    HashSet<Student> setStudenti = new HashSet<>();
+    setStudenti.add(s1); setStudenti.add(s2); setStudenti.add(s3); setStudenti.add(s4); setStudenti.add(s5);
+
+    System.out.println("E Andrei prezent? " + s3.studentPrezent(listaStudenti));
+    System.out.println("E Ioana prezenta? " + s5.studentPrezentSet(setStudenti));
+
 }
--- src/main/java/ro/ulbs/proiectaresoftware/students/Student.java
+++ src/main/java/ro/ulbs/proiectaresoftware/students/Student.java
@@ -1,5 +1,7 @@
 package ro.ulbs.proiectaresoftware.students;
 import java.util.Objects;
+import java.util.HashSet;
+import java.util.ArrayList;
 
 public class Student {
     protected int numarMatricol;
@@ -34,5 +36,24 @@ public class Student {
     public String toString() {
         return String.format("%-5d | %-15s | %-15s | %-10s",
                 numarMatricol, prenume, nume, formatieDeStudiu);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return numarMatricol == student.numarMatricol && Objects.equals(prenume, student.prenume) && Objects.equals(nume, student.nume) && Objects.equals(formatieDeStudiu, student.formatieDeStudiu);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numarMatricol, prenume, nume, formatieDeStudiu);
+    }
+
+    public boolean studentPrezent(ArrayList<Student> lista){
+        return lista != null && lista.contains(this);
+    }
+    public boolean studentPrezentSet(HashSet<Student> set){
+        return set != null && set.contains(this);
     }
 }
